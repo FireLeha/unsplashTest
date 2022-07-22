3a0ffeff8c8fdd56faddcaf50e66bb616bcfb515
FireLeha <zegger1@gmail.com>
2022-07-22 14:02:42 +0300

    Initial commit

---
+++ app/src/main/java/com/vantula/unsplashtest/view/CategoriesFragment.kt
@@ -0,0 +1,6 @@
+package com.vantula.unsplashtest.view
+
+import androidx.fragment.app.Fragment
+
+class CategoriesFragment: Fragment() {
+}
---
+++ app/src/main/java/com/vantula/unsplashtest/viewmodel/CategoriesViewModel.kt
@@ -0,0 +1,6 @@
+package com.vantula.unsplashtest.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class CategoriesViewModel: ViewModel() {
+}
